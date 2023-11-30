ba2bc60b61d013df14be0469bfdcb6dacac1b865
Def0x00 <137518061+DefNu1l@users.noreply.github.com>
2023-11-30 11:14:18 +0100

    included usage of configvalidate

--- test/test.c
+++ test/test.c
@@ -3,15 +3,25 @@
 
 int main(void) {
 
-	int storage_count;
-	init_t *storage = configinit("test.txt", &storage_count);
+	const char *config_file = "test.txt";
+	
 
-	for (int idx = 0; idx < storage_count; idx++) {
-		printf("KEY: %s\nVALUE: %s\n",
-			storage[idx].get_key, storage[idx].get_value);
+	int check = configvalidate(config_file, 0);
+	if (check != 1) {
+		int storage_count;
+		init_t *storage = configinit(config_file, &storage_count);
+
+		for (int idx = 0; idx < storage_count; idx++) {
+			printf("KEY: %s\nVALUE: %s\n",
+				storage[idx].get_key, storage[idx].get_value);
+		}
+
+		configcleanup(storage, storage_count);
 	}
-
-	configcleanup(storage, storage_count);
+	else {
+		puts("Check failed");
+		return -1;
+	}
 
 	return 0;
 }
